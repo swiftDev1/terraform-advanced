8a2398cb67a1a1d72604207d79c8f53ca0201cb2
Chika Hope <elumeziemchikahope@gmail.com>
2023-12-11 10:11:09 +0100

    changed terraform version

--- main.tf
+++ main.tf
@@ -1,6 +1,13 @@
-module "s3-website" {
-  source = "./modules/static-website"
-  bucket-name = var.bucket-name
-  tags = var.tags
+import {
+  to = aws_instance.web-server
+  id = "i-01f743c6bd8da6d1c"
+}
 
+resource "aws_instance" "web-server" {
+    ami = "ami-0fc5d935ebf8bc3bc"
+    instance_type = "t2.micro"
+    tags = {
+      "Name" = "Imported-Instance"
+    }
+  
 }
